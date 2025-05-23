608ca6aafa471c7002c072c33401f8b3e09ce940
YasamanBehnamww <yasamanbehnam.wgit@gmail.com>
2025-05-23 16:43:07 +0330

    quiz box

--- src/index.js
+++ src/index.js
@@ -13,3 +13,19 @@ close.addEventListener('click', () => {
     sidebar.style.opacity = 0
     sidebar.style.translate = "100%";
 })
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
